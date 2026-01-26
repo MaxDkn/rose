8c8da45d6149abcb3f3053008ebf866eebc126ae
maxdkn <max.deckmyn@gmail.com>
2026-01-26 19:22:27 +0100

    Updates

---
+++ _next/static/chunks/91d44436f7e67462.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,76445,t=>{t.v(o=>Promise.all(["static/chunks/37d7043101d49484.js"].map(o=>t.l(o))).then(()=>o(95425)))}]);
